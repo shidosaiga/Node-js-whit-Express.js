d8b46508d18940c67451bbda9cd92bf7173364ef
Apirak Kaewpachum <shidosaigaapirak@gmail.com>
2023-03-30 15:21:44 +0700

    Update3

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const part = require('path')
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT; // || 4000
 
 app.use(morgan('combined'));
-app.use(express.static(part.join(__dirname,"/public/")))
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.set("views","src/views");
+app.set("view engine","ejs");
+
 
 app.get("/",(req,res)=>{
-    res.send("undefind file");
+    res.render('index',{username: 'Apirak Kaewpachum'});
 })
 
 app.listen(PORT, ()=>{
-    debug("Listening on PORT ",PORT)
+    debug("Listening on PORT ",PORT);
+    console.log("http://localhost:3000/")
 })
 
