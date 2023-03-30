9c5c0300f077705ca8f14d6ce5b75c5df3887d57
Apirak Kaewpachum <shidosaigaapirak@gmail.com>
2023-03-30 13:58:39 +0700

    update template web 1

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express();
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const part = require('path')
+
+const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(part.join(__dirname,"/public/")))
 
 app.get("/",(req,res)=>{
-    res.send("Hello apirak kaewpachum");
+    res.send("undefind file");
 })
 
 app.listen(port, ()=>{
