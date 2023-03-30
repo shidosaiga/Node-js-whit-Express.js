c1159615b407a8c81926f92445a3585233d55b93
Apirak Kaewpachum <shidosaigaapirak@gmail.com>
2023-03-30 17:24:48 +0700

    update add products page

--- app.js
+++ app.js
@@ -2,23 +2,35 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path')
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT; // || 4000
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","src/views");
-app.set("view engine","ejs");
+app.set("views", "src/views");
+app.set("view engine", "ejs");
 
-
-app.get("/",(req,res)=>{
-    res.render('index',{username: 'Apirak Kaewpachum'});
+productRouter.route("/").get((req, res) => {
+    res.render('products');
 })
 
-app.listen(PORT, ()=>{
-    debug("Listening on PORT ",PORT);
+productRouter.route("/1").get((req, res) => {
+    res.send("Product1")
+})
+
+app.get("/", (req, res) => {
+    res.render('index', { username: 'Apirak Kaewpachum', customer: ["A1", "AB5", "APirak", "A18Ac"] });
+})
+
+app.use("/products", productRouter)
+
+
+
+app.listen(PORT, () => {
+    debug("Listening on PORT ", PORT);
     console.log("http://localhost:3000/")
 })
 
