31d222cdae2ea5a5eb83ebb3d8a0b277b0a2db07
Apirak Kaewpachum <shidosaigaapirak@gmail.com>
2023-03-30 19:44:54 +0700

    update add product in index

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path')
-const productRouter = express.Router();
-const products = require("./data/products.json")
 
 const app = express();
 const PORT = process.env.PORT; // || 4000
+const productsRouter = require("./src/router/productsRouter")
+const products = require("./src/data/products.json")
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -14,22 +14,12 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-    res.render('products',
-        products,
-    );
-})
-
-
-// productRouter.route("/1").get((req, res) => {
-//     res.send("Product1")
-// })
 
 app.get("/", (req, res) => {
-    res.render('index', { username: 'Apirak Kaewpachum', customer: ["A1", "AB5", "APirak", "A18Ac"] });
+    res.render('index',{products} );
 })
 
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 
 
