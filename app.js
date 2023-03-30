9033217169a40f18e9a98b360d4496a8d57a949f
Apirak Kaewpachum <shidosaigaapirak@gmail.com>
2023-03-30 18:27:13 +0700

    update add Json products

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path')
 const productRouter = express.Router();
+const products = require("./data/products.json")
 
 const app = express();
 const PORT = process.env.PORT; // || 4000
@@ -14,12 +15,15 @@ app.set("views", "src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-    res.render('products');
+    res.render('products',
+        products,
+    );
 })
 
-productRouter.route("/1").get((req, res) => {
-    res.send("Product1")
-})
+
+// productRouter.route("/1").get((req, res) => {
+//     res.send("Product1")
+// })
 
 app.get("/", (req, res) => {
     res.render('index', { username: 'Apirak Kaewpachum', customer: ["A1", "AB5", "APirak", "A18Ac"] });
